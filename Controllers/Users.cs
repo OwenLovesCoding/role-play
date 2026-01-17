f46546fe24c48b342229dc28d94e35588b64fa5a
unknown <iraoyaowen5@gmail.com>
2026-01-18 00:18:22 +0100

    added the user tb

---
+++ Controllers/Users.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using role_play.Models;
+
+namespace role_play.Controllers
+{
+    [Route("api/[controller]")]
+    public class Users(UserContext context): ControllerBase
+    {
+        private readonly UserContext _context = context;
+
+
+        [HttpGet("all-users")]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.ToList();
+           
+            return Ok(users);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using role_play.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// connect db to the container.
+// Add DbContext with your database provider
+builder.Services.AddDbContext<UserContext>(options =>
+{
+    // Choose ONE based on your database:
+
+    // For SQL Server:
+    options.UseSqlServer(builder.Configuration["SQLServer:DefaultConnection"]);
+
+    // For SQLite:
+    // options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    // For PostgreSQL:
+    // options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    // For MySQL:
+    // options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
+    //     new MySqlServerVersion(new Version(8, 0, 0)));
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -14,7 +36,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
