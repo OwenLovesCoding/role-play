b0d680495e19a393afe774605a4452244a21709a
unknown <iraoyaowen5@gmail.com>
2026-01-19 19:10:09 +0100

    removed all occurence of connection string

--- Program.cs
+++ Program.cs
@@ -9,10 +9,7 @@ using Resend;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// connect db to the container.
-// Add DbContext with your database provider
 
-//var connectionString = "Host=shortline.proxy.rlwy.net;Port=49418;Database=railway;Username=postgres;Password=yFwomSrjwHWhSZFzzyMppoKhrNZqncQd;SSL Mode=Require;Trust Server Certificate=true";
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -23,19 +20,10 @@ if (!string.IsNullOrEmpty(connectionString))
 
 builder.Services.AddDbContext<UserContext>(options =>
 {
-    // Choose ONE based on your database:
-
-    // For SQL Server:
-    //options.UseSqlServer(builder.Configuration["SQLServer:DefaultConnection"]);
-
-    // For SQLite:
-    // options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-
+    
     // For PostgreSQL:
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-    // For MySQL:
-    // options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
-    //     new MySqlServerVersion(new Version(8, 0, 0)));
+    
 });
 
 
@@ -50,8 +38,6 @@ builder.Services.Configure<ResendClientOptions>(o => {
     o.ApiToken = builder.Configuration["Resend:ApiKey"]!;
 });
 builder.Services.AddTransient<IResend, ResendClient>();
-//builder.Services.AddTransient<IResend>(sp =>
-//ResendClient.Create(builder.Configuration["Resend:ApiKey"]!));
 
 var app = builder.Build();
 
